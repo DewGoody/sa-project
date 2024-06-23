75850a464c75e3e9611d4433fde56d329169379d
DewGoody <dewzky1@gmail.com>
2024-06-24 01:16:11 +0700

    create rordor and prakan

---
+++ app/components/header/page.js
@@ -0,0 +1,13 @@
+// components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 p-4 text-white text-center">
+      <h1 className="text-3xl">Department of Student Affairs</h1>
+      <h2 className="text-xl">Accidental Compensation Claim Form</h2>
+    </header>
+  );
+};
+
+export default Header;
