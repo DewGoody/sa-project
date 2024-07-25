548e4ac95f6989f6775185c824b0102f0824f3fd
DewGoody <dewzky1@gmail.com>
2024-07-25 20:21:19 +0700

    edit

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors');
+const db = require('./db');
+const {createPdfUsingAccForm} = require('./models/accForm')
+
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.post('/api/create', (req, res, next) => {
+    let data = req.body
+
+    createPdfUsingAccForm(data)
+    
+    db.query("INSERT INTO user VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", [data.id, data.name, data.faculty, data.phone, data.desAcc, data.email, data.desInj, data.dateAcc, data.placeAcc, data.placeTreat, data.typeHos, data.medicalFeeNum, data.bankAcc, data.medicalFeeText],(err, result) => {
+        if(err){
+            console.log(err);
+        }
+    })
+
+    res.status(200).send({
+        msg: "ok"
+    })
+});
+
+app.get('/api', (req, res, next) => {
+    res.status(200).send({
+        msg: "ok from server!"
+    })
+});
+
+app.listen(1337, () => console.log("Server is running at port 1337"))
