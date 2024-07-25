c864235cf91758d6438d993a1f387888c2f4844b
DewGoody <dewzky1@gmail.com>
2024-07-25 19:46:37 +0700

    delete

--- app/components/ServiceCard.js
+++
@@ -1,8 +0,0 @@
-function ServiceCard({ title, icon }) {
-    return (
-      <div className="flex justify-between  items-center border rounded p-4">
-        <p className="text-gray-700">{title}</p>
-        <div className="text-2xl">{icon}</div>
-      </div>
-    );
-  }
