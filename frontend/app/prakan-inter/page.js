4707e063fc298acd44bf26b1560b9d99c7338529
Panthawit Khantipan <panthawit.k@gmail.com>
2024-10-17 23:33:31 +0700

    Prakan inter page HTML Done

--- frontend/app/prakan-inter/page.js
+++ frontend/app/prakan-inter/page.js
@@ -134,7 +134,96 @@ function page() {
                     ></textarea>
                   </div>
                 </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pt-4 ">
+                  <div>
+                    <label className="block text-gray-700 mb-2">
+                      Place of treatment
+                    </label>
+                    <input
+                      type="text"
+                      name="placeOfTreatment"
+                      //value={formData.citizenId}
+                      // onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+                      placeholder="Place of treatment"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700 mb-2">
+                      Type of hospital
+                    </label>
+                    <input
+                      type="text"
+                      name="faculty"
+                      // value={formData.birthDate}
+                      //onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+                      placeholder="TODO : Drop Downlist here"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700 mb-2">
+                      Date of accident
+                    </label>
+                    <input
+                      type="text"
+                      name="phoneNumber"
+                      //value={formData.religion}
+                      //onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+                      placeholder="TODO : Datepicker here"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700 mb-2">
+                      Place of accident
+                    </label>
+                    <input
+                      type="text"
+                      name="email"
+                      //value={formData.ethnicity}
+                      //onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+                      placeholder="Place of accident"
+                    />
+                  </div>
+                </div>
+                <h3 className="text-lg font-semibold my-4">
+                  Net of medical fee total amount
+                </h3>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label className="block text-gray-700 mb-2">
+                      Medical fee (In number)
+                    </label>
+                    <input
+                      type="text"
+                      name="placeOfTreatment"
+                      //value={formData.citizenId}
+                      // onChange={handleChange}
+                      className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+                      placeholder="Medical fee (In number)"
+                    />
+                  </div>
+                </div>
               </section>
+
+              <div className="flex justify-between mt-8">
+                {/*TODO Edit Link*/}
+                <a href="/home">
+                  <button className="bg-gray-400 hover:bg-ping-400 text-white font-bold py-2 px-4 rounded-md mb-11">
+                    Back
+                  </button>
+                </a>
+                <a href="./prakan/checkPrakan">
+                  <button
+                    //onClick={handleSubmit}
+                    className="bg-pink-300 hover:bg-ping-400 text-white font-bold py-2 px-4 rounded-md mb-11"
+                  >
+                    Next
+                  </button>
+                </a>
+              </div>
             </div>
           </main>
         </div>
