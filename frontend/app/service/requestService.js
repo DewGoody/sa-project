a0618ac2899a6788f82cc991cb09ba3e70e573be
Chakrit Tangpanupong <Ctangpanupong@gmail.com>
2025-02-17 03:27:44 +0700

    Wantinfo comeback

--- frontend/app/service/requestService.js
+++ frontend/app/service/requestService.js
@@ -308,6 +308,22 @@ export async function changeStatusToSucc(id) {
         throw { code: 400, error: new Error("Bad Request") }
     }
 }
+export async function changeStatusToWantInfo(id) {
+    if (id) {
+        const request = await getRequestByIdFast({ id: id })
+        if (request.status !== "ส่งเอกสารแล้ว" && request.status !== "รอเจ้าหน้าที่ดำเนินการ") {
+            throw { code: 400, error: new Error("Bad Request") }
+        }
+        const changeStatusRequest = await prisma.request.update({
+            where: { id: request.id },
+            data: { status: "ขอข้อมูลเพิ่มเติม" }
+        })
+        return changeStatusRequest
+    }
+    else {
+        throw { code: 400, error: new Error("Bad Request") }
+    }
+}
 
 
 export async function changeStatusToAll(ids, status) {
