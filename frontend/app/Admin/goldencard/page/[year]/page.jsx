44fa5e6edad1f38b2118722153925fa290143972
Chakrit Tangpanupong <phoo@Chakrits-MacBook-Air.local>
2025-03-26 17:21:14 +0700

    try to debug modal

--- frontend/app/Admin/goldencard/page/[year]/page.jsx
+++ frontend/app/Admin/goldencard/page/[year]/page.jsx
@@ -809,14 +809,15 @@ const App = () => {
                     >
                         <textarea
                             style={{ width: "100%", height: "200px", border: "gray solid", borderRadius: "15px", padding: "15px", fontSize: "18px" }}
-
                             onChange={(e) => setMoreInfoValue(e.target.value)}
-                        />
+
+                        >
                         {Data.map((item) => {
                             if (item.reqId === reqMoreInfo) {
                                 return item.more_info
                             }
                         })}
+                        </textarea>
                     </Modal>
 
                 </Content>
