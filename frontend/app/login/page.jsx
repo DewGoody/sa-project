6425c61d11b5a50c4a3cd3d8ed2ceb6b6fafe98f
Chakrit Tangpanupong <Ctangpanupong@gmail.com>
2025-04-18 04:00:23 +0700

    merge dev

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -7,17 +7,17 @@ const login = () => {
     useEffect(() => {
         // เรียก API logout เพื่อเคลียร์คุกกี้เมื่อเข้าหน้า login
         axios.get('/api/user/logout')
-          .then(response => {
-            console.log('Logged out and cookies cleared');
-          })
-          .catch(error => {
-            console.error('Error logging out:', error);
-          });
-      }, []);
+            .then(response => {
+                console.log('Logged out and cookies cleared');
+            })
+            .catch(error => {
+                console.error('Error logging out:', error);
+            });
+    }, []);
     return (
         <div className="h-screen">
             <div className="flex justify-center items-end h-1/6 ">
-              
+
             </div>
 
             <div className="flex justify-center items-center h-4/6 space-x-6">
@@ -25,25 +25,26 @@ const login = () => {
                     <div className='mb-10'>
                         <p className="text-2xl font-semibold">เข้าสู่ระบบ</p>
                         <p className="text-2xl font-semibold">(Login)</p>
-                     </div>
+                    </div>
                     <div className="w-3/4 h-14 ">
                         <a href="/login/Admin" className="w-full h-full p-8 bg-pink-400 flex items-center justify-center rounded-2xl font-semibold text-white">
-                        <div>
+                            <div>
                                 <p className=''>เข้าสู่ระบบสำหรับเจ้าหน้าที่</p>
                                 <p className="text-sm">(Staff login)</p>
-                        </div> 
+                            </div>
                         </a>
                         {/* Login page admin */}
                     </div>
                     <div className="w-3/4 h-14">
-                        <a href="https://cunex-auth-uat.azurewebsites.net/?partnerid=cuserv" className="mt-8 w-full h-full p-8 bg-pink-400  flex flex-col items-center justify-center space-y-4 rounded-2xl font-semibold text-white">
+                        {/* <a href="https://cunex-auth.azurewebsites.net/?partnerid=cuserv" className="w-full h-full bg-pink-400 flex items-center justify-center rounded-2xl">เข้าสู่ระบบสำหรับนิสิต</a> */}
+                        {/* SSO */}
+                        <a href="https://cunex-auth-uat.azurewebsites.net/?partnerid=cuserv"  className="mt-8 w-full h-full p-8 bg-pink-400  flex flex-col items-center justify-center space-y-4 rounded-2xl font-semibold text-white">
                             <div>
                                 <p className=''>เข้าสู่ระบบสำหรับนิสิต</p>
                                 <p className="text-sm">(Student login)</p>
                             </div>
                         </a>
-                        {/* <a href="https://cunex-auth.azurewebsites.net/?partnerid=cuserv" className="w-full h-full bg-pink-400 flex items-center justify-center rounded-2xl">เข้าสู่ระบบสำหรับนิสิต</a> */}
-                        {/* SSO */}
+
                     </div>
                 </div>
                 <div className=" h-5/6 w-5/12 shadow-center-lg rounded-3xl items-center place-content-center flex max-[700px]:hidden">
